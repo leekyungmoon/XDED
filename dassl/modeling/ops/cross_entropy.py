1063347b2be5947fa18562becc52d0fa8eea5622
Kyungmoon Lee <kyungmoon@postech.ac.kr>
2022-11-15 17:52:05 +0900

    Update cross_entropy.py
    added XDED code for semantic segmentation

--- dassl/modeling/ops/cross_entropy.py
+++ dassl/modeling/ops/cross_entropy.py
@@ -40,3 +40,39 @@ class KDLoss(torch.nn.Module):
         q = torch.softmax(target/self.temp_factor, dim=1)
         loss = self.kl_div(log_p, q)*(self.temp_factor**2)/input.size(0)
         return loss
+
+class pixelwise_XDEDLoss(torch.nn.Module):
+    def __init__(self, temp_factor=2.0):
+        super(pixelwise_XDEDLoss, self).__init__()
+        self.temp_factor = temp_factor
+        self.kl_div = torch.nn.KLDivLoss(reduction="sum")
+        self.CLASS_NUM = 19
+
+    def xded_loss(self, input, target):
+        log_p = torch.log_softmax(input/self.temp_factor, dim=1)
+        q = torch.softmax(target/self.temp_factor, dim=1)
+        loss = self.kl_div(log_p, q)*(self.temp_factor**2)/input.size(0)
+        return loss
+
+    def forward(self, main_out, gts):
+        # main_out.shape : [batch, 19, 768, 768]
+        # gts.shape : [batch, 768, 768]
+
+        batch_size = main_out.shape[0]
+
+        flat_gts = gts.reshape(-1) # [batch*768*768]
+        flat_out = main_out.reshape(-1, self.CLASS_NUM)
+
+        flat_targets = main_out.clone().detach().reshape(-1, self.CLASS_NUM)
+        # [batch*768*768, 19]
+
+        flat_gt_set = flat_gts.unique().tolist()
+        ensemble_dict= {}
+
+        for f_gt in flat_gt_set:
+            if f_gt == 255:
+                continue
+            cur_gt_idx = flat_gts == f_gt # [False, True, ...]
+            flat_targets[cur_gt_idx, :] = flat_out[cur_gt_idx].mean(0).detach()
+
+        return self.xded_loss(flat_out, flat_targets)

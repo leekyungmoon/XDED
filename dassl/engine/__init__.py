abab9b81a30810cf6c4daffa8c8c4dc09ed8bd11
Kyungmoon Lee <kyungmoon@postech.ac.kr>
2023-09-11 05:12:34 +0900

    Update __init__.py

--- dassl/engine/__init__.py
+++ dassl/engine/__init__.py
@@ -1,5 +1,5 @@
 from .build import TRAINER_REGISTRY, build_trainer # isort:skip
-from .trainer import TrainerX, TrainerXU, TrainerBase, SimpleTrainer, SimpleNet # isort:skip
+from .trainer import TrainerX, TrainerBase, SimpleTrainer, SimpleNet # isort:skip
 
 from .xded import *
 
